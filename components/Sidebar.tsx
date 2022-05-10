e719ad96f6683587505bc9beee831ef0dd54705d
paakowden <paakow1114@gmail.com>
2022-05-10 04:22:25 -0700

    nextapp-twitter2.0-clone

---
+++ components/Sidebar.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import {
+  BellIcon,
+  HashtagIcon,
+  BookmarkIcon,
+  CollectionIcon,
+  DotsCircleHorizontalIcon,
+  MailIcon,
+  UserIcon,
+  HomeIcon,
+} from '@heroicons/react/outline'
+
+function Sidebar() {
+  return (
+    <div>
+      <img
+        className="h-10 w-10"
+        src="https://links.papareact.com/drq"
+        alt="twit"
+      />
+    </div>
+  )
+}
+
+export default Sidebar
---
+++ pages/index.tsx
@@ -0,0 +1,26 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+import Sidebar from '../components/Sidebar'
+
+const Home: NextPage = () => {
+  return (
+    <div className="">
+      <Head>
+        <title>Twitter 2.0</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        {/* Sidebar */}
+        <Sidebar />
+
+        {/* Feed */}
+
+        {/*Widgets */}
+      </main>
+    </div>
+  )
+}
+
+export default Home
